e44450d0fdad27bcc62c823e29e2ea9eb0b26182
rossp <rossp@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2011-02-11 02:35:45 +0000

    Update Zope2 interface

--- Products/PloneServicesCenter/validators.py
+++ Products/PloneServicesCenter/validators.py
@@ -1,14 +1,15 @@
-from Products.validation.interfaces import ivalidator
+from zope.interface import implements
+
+from Products.validation.interfaces.IValidator import IValidator
+
 
 class IndustriesValidator:
-
-    __implements__=(ivalidator,)
+    implements(IValidator)
 
     def __init__(self, name):
         self.name = name
 
     def __call__(self, value, *args, **kwargs):
-        if len([ v for v in value if v ]) > 2:
+        if len([v for v in value if v]) > 2:
             return 'Please select at most two industries'
         return True
-
