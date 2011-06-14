32043d37ba55454117fd9730dfe959fe000c5322
macagua <macagua@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2011-06-14 18:26:53 +0000

    Added improvements about the Internationalization

--- Products/PloneServicesCenter/validators.py
+++ Products/PloneServicesCenter/validators.py
@@ -1,6 +1,7 @@
 from zope.interface import implements
 
 from Products.validation.interfaces.IValidator import IValidator
+from Products.PloneServicesCenter import PSCMessageFactory as _
 
 
 class IndustriesValidator:
@@ -11,5 +12,5 @@ class IndustriesValidator:
 
     def __call__(self, value, *args, **kwargs):
         if len([v for v in value if v]) > 2:
-            return 'Please select at most two industries'
+            return _(u'Please select at most two industries')
         return True
